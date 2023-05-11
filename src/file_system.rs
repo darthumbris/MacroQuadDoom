d7b05ac1249956ee5d934ac554b9ae6e8fe8049b
Siebe Hoogenboom <shoogenb@student.codam.nl>
2023-05-11 11:54:18 +0200

    added a file_system class for handling the reading of files

---
+++ src/file_system.rs
@@ -0,0 +1,19 @@
+pub struct FileSystem {
+    //TODO
+}
+
+impl FileSystem {
+    pub fn check_num_for_name(&self, name: &String) -> i32 {
+        //TODO
+        -1
+    }
+
+    pub fn file_length(&self, lump: i32) -> i32 {
+        //TODO
+        -1
+    }
+
+    pub fn open_file_reader(&self) {
+        //TODO   
+    }
+}
